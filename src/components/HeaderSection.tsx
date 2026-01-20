144b528e6ff81c892db325fc8e10da97ea05f7ef
faturgautama <faturgautama.dev@gmail.com>
2026-01-20 10:24:30 +0700

    feat: implement header section component

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -1,22 +1,48 @@
 import React from 'react';
+import Checkbox from './Checkbox';
+import { DIMENSIONS, COLORS, TYPOGRAPHY } from '../constants';
 
 interface HeaderSectionProps {
   isChecked: boolean;
   onCheckboxChange: () => void;
-  animationState: number;
 }
 
 /**
- * Displays "All pages" label and master checkbox
+ * HeaderSection component displaying "All pages" label and master checkbox
  */
 const HeaderSection: React.FC<HeaderSectionProps> = ({ 
   isChecked, 
-  onCheckboxChange, 
-  animationState 
+  onCheckboxChange 
 }) => {
+  const containerStyle: React.CSSProperties = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: `${DIMENSIONS.headerSection.paddingTop}px`,
+    paddingRight: `${DIMENSIONS.headerSection.paddingRight}px`,
+    paddingBottom: `${DIMENSIONS.headerSection.paddingBottom}px`,
+    paddingLeft: `${DIMENSIONS.headerSection.paddingLeft}px`,
+  };
+
+  const textStyle: React.CSSProperties = {
+    fontFamily: TYPOGRAPHY.fontFamily,
+    fontWeight: TYPOGRAPHY.fontWeight,
+    fontSize: `${TYPOGRAPHY.fontSize}px`,
+    lineHeight: TYPOGRAPHY.lineHeight,
+    letterSpacing: `${TYPOGRAPHY.letterSpacing}px`,
+    verticalAlign: TYPOGRAPHY.verticalAlign,
+    color: COLORS.textPrimary,
+    margin: 0,
+  };
+
   return (
-    <div>
-      {/* Component implementation will be added in subsequent tasks */}
+    <div style={containerStyle}>
+      <span style={textStyle}>All pages</span>
+      <Checkbox
+        id="all-pages-checkbox"
+        isChecked={isChecked}
+        onChange={onCheckboxChange}
+      />
     </div>
   );
 };
--- src/components/PageSelectionComponent.tsx
+++ src/components/PageSelectionComponent.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainWrapper from './MainWrapper';
 import DividerLine from './DividerLine';
+import HeaderSection from './HeaderSection';
 
 /**
  * Root component for the page selection interface
  * Manages state for all checkboxes and automatic "All pages" logic
  */
 const PageSelectionComponent: React.FC = () => {
+  const [allPagesSelected, setAllPagesSelected] = useState(false);
+
+  const handleAllPagesCheckboxChange = () => {
+    setAllPagesSelected(!allPagesSelected);
+  };
+
   return (
     <MainWrapper>
-      <div style={{ padding: '20px', textAlign: 'center' }}>
-        MainWrapper Component Test
-      </div>
+      <HeaderSection
+        isChecked={allPagesSelected}
+        onCheckboxChange={handleAllPagesCheckboxChange}
+      />
       <DividerLine />
       <div style={{ padding: '20px', textAlign: 'center' }}>
         DividerLine Component Test

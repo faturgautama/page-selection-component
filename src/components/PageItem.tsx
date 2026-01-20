681fdb0e96f87e82fe41f1af690739f8560c3716
faturgautama <faturgautama.dev@gmail.com>
2026-01-20 10:26:48 +0700

    feat: implement page item component

--- src/components/PageItem.tsx
+++ src/components/PageItem.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
+import Checkbox from './Checkbox';
+import { DIMENSIONS, COLORS, TYPOGRAPHY } from '../constants';
 
 interface PageItemProps {
   pageNumber: number;
   isChecked: boolean;
   onChange: () => void;
-  animationState: number;
 }
 
 /**
- * Individual page row with label and checkbox
+ * PageItem component displaying individual page label and checkbox
  */
 const PageItem: React.FC<PageItemProps> = ({ 
   pageNumber, 
   isChecked, 
-  onChange, 
-  animationState 
+  onChange 
 }) => {
+  const containerStyle: React.CSSProperties = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: `${DIMENSIONS.pageItem.paddingTop}px`,
+    paddingRight: `${DIMENSIONS.pageItem.paddingRight}px`,
+    paddingBottom: `${DIMENSIONS.pageItem.paddingBottom}px`,
+    paddingLeft: `${DIMENSIONS.pageItem.paddingLeft}px`,
+  };
+
+  const textStyle: React.CSSProperties = {
+    fontFamily: TYPOGRAPHY.fontFamily,
+    fontWeight: TYPOGRAPHY.fontWeight,
+    fontSize: `${TYPOGRAPHY.fontSize}px`,
+    lineHeight: TYPOGRAPHY.lineHeight,
+    letterSpacing: `${TYPOGRAPHY.letterSpacing}px`,
+    verticalAlign: TYPOGRAPHY.verticalAlign,
+    color: COLORS.textPrimary,
+    margin: 0,
+  };
+
   return (
-    <div>
-      {/* Component implementation will be added in subsequent tasks */}
+    <div style={containerStyle}>
+      <span style={textStyle}>Page {pageNumber}</span>
+      <Checkbox
+        id={`page-${pageNumber}-checkbox`}
+        isChecked={isChecked}
+        onChange={onChange}
+      />
     </div>
   );
 };
